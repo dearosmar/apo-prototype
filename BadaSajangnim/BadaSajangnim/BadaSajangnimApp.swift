e7825be71e8e67f170ea7bb195bc7c4ab0ad3963
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-08-02 16:08:24 +0900

    [chore] #38 Xcode 프로젝트 추가 + Info.plist 연결 — NSAllowsLocalNetworking·카메라 권한 문구

---
+++ BadaSajangnim/BadaSajangnim/BadaSajangnimApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadaSajangnimApp.swift
+//  BadaSajangnim
+//
+//  Created by h2e on 7/30/26.
+//
+
+import SwiftUI
+
+@main
+struct BadaSajangnimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
