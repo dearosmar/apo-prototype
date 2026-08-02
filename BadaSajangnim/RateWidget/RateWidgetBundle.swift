36b8da5bc880ef6437d2632d46aa789abab97c94
JUHUI CHOI <dear.ros.mar@gmail.com>
2026-08-02 16:33:59 +0900

    [chore] #40 Widget Extension 타겟 추가 (RateWidget)

---
+++ BadaSajangnim/RateWidget/RateWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  RateWidgetBundle.swift
+//  RateWidget
+//
+//  Created by h2e on 8/2/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RateWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RateWidget()
+    }
+}
